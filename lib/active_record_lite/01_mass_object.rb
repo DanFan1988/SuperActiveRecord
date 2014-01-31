770749dfefe14b4aee24c090c7678fcf5b3ce6c6
Dan Fan <dan.fan1988@gmail.com>
2014-01-30 18:08:34 -0800

    fixed up associations

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -2,14 +2,26 @@ require_relative '00_attr_accessor_object.rb'
 
 class MassObject < AttrAccessorObject
   def self.my_attr_accessible(*new_attributes)
-    # ...
+    new_attributes = new_attributes.map { |attr| attr.to_sym }
+    	self.attributes.concat(new_attributes)
   end
 
   def self.attributes
-    # ...
+    if self == MassObject
+    	raise "don't call attributes on MassObject"
+    else
+    	@attributes ||= []
+    end
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+    	attr_name = attr_name.to_sym
+    	if self.class.attributes.include?(attr_name)
+    		self.send("#{attr_name}", value)
+    	else
+    		raise "can't mass assign"
+    	end
+    end
   end
 end
--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -20,13 +20,29 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    default = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    default.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    default = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    default.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
